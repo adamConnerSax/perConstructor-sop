3ded8f815d12fefdb6630b10a962035700a87057
adamConnerSax <adam_conner_sax@yahoo.com>
2017-04-22 13:28:30 -0400

    Added functionPOPFromClass to distribute

--- src/Generics/SOP/Distribute.hs
+++ src/Generics/SOP/Distribute.hs
@@ -7,10 +7,11 @@
 {-# LANGUAGE RankNTypes                 #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE TypeOperators              #-}
-
+{-# LANGUAGE ConstraintKinds            #-}
 module Generics.SOP.Distribute where
 
 import           Generics.SOP hiding (Compose)
+import           Generics.SOP.Dict (Dict,withDict)
 
 expand::forall (f :: [k] -> *) xs.(SListI xs)=>NS f xs -> NP (Maybe :.: f) xs
 expand ns = go sList (Just ns) where
@@ -47,11 +48,11 @@ shiftWrappedInjection:: Functor g=>WrappedInjection g f xs a -> WrappedInjection
 shiftWrappedInjection (Fn f) = Fn $ K . fmap S . unK . f
 
 -- NB: For applicative h, this is an inverse of hsequence.  If h is not applicative, then this is not invertible.
-distribute::(Functor h, SListI xs)=>h (NP g xs) -> NP (h :.: g) xs
-distribute x = hap wrappedProjections (hpure $ K x)
+distributeNP::(Functor h, SListI xs)=>h (NP g xs) -> NP (h :.: g) xs
+distributeNP x = hap wrappedProjections (hpure $ K x)
 
-distributeI::(Functor h, SListI xs)=>h (NP I xs) -> NP h xs
-distributeI = hmap (fmap unI . unComp) . distribute
+distributeI_NP::(Functor h, SListI xs)=>h (NP I xs) -> NP h xs
+distributeI_NP = hmap (fmap unI . unComp) . distributeNP
 
 functorToNP::forall g a.(Functor g,Generic a)=>g a -> NP (g :.: (Maybe :.: NP I)) (Code a)
 functorToNP ga = hap wrappedProjections (hpure $ K (expandA <$> ga))
@@ -65,13 +66,20 @@ reAssociateNP = hmap reAssociate
 distributeToFields::(Functor g, SListI2 xss)=>NP ((g :.: Maybe) :.: NP I) xss -> POP (g :.: Maybe) xss
 distributeToFields =
   let proxyC = Proxy :: Proxy SListI
-  in POP . hcliftA proxyC (distributeI . unComp)
+  in POP . hcliftA proxyC (distributeI_NP . unComp)
 
 reconstructA::(Functor h, Generic a) => NP (h :.: NP I) (Code a) -> NP (K (h a)) (Code a)
 reconstructA = hliftA (K . fmap (to . SOP) . unK) . hap wrappedInjections
 
+functionPOPFromClass::forall c f g xss.SListI2 xss=>Dict (All2 c) xss->(forall a.c a=>f a -> g a)->POP (f -.-> g) xss
+functionPOPFromClass d fn = withDict d $ hcpure (Proxy :: Proxy c) $ Fn fn
 
-
-
+{-
+functionPOPFromClass'::forall c f g xss.(All2 c xss, SListI2 xss)=>(forall a.c a=>f a -> g a)->POP (f -.-> g) xss
+functionPOPFromClass' fn =
+  let dict :: Dict (All2 c) xss
+      dict = all_POP hdicts
+  in  withDict dict $ hcpure (Proxy :: Proxy c) $ Fn fn
+-}
 
   

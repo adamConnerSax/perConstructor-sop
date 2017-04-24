2eff06f2a89971f09b4bf752be4066c80786a781
adamConnerSax <adam_conner_sax@yahoo.com>
2017-04-24 09:17:05 -0400

    Working on documentation for DMapUtilities.hs

--- src/Generics/SOP/DMapUtilities.hs
+++ src/Generics/SOP/DMapUtilities.hs
@@ -9,7 +9,44 @@
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
 {-# LANGUAGE RankNTypes            #-}
-module Generics.SOP.DMapUtilities where
+{-|
+Module           : Generics.SOP.DMapUtilities
+Description      : Utilities for converting between the NS/NP types of generics-sop and Dependent Maps.
+Copyright        : Adam Conner-Sax, 2017
+License          : BSD-style (see LICENSE)
+Maintainer       : Adam Conner-Sax (adam_conner_sax@yahoo.com)
+Stability        : Experimental
+-}
+
+module Generics.SOP.DMapUtilities
+  (
+    -- * Type Functions
+    -- ** Add functors around types in a typelist or typelist of typelists
+    FunctorWrapTypeList
+  , FunctorWrapTypeListOfLists
+  
+    -- * Types
+  , TypeListTag
+  
+    -- * Conversions
+    -- ** NP <-> DMap
+  , npToDMap
+  , dMapToNP
+    -- ** NS <-> DSum
+  , nsToDSum
+  , dSumToNS
+  
+  -- * Functor wrapping/unwrapping utilities for 'NP' 
+  , npUnCompose
+  , npReCompose
+  , nsOfnpReCompose
+  
+  -- * Utilities
+  , npSequenceViaDMap
+  -- * Proofs
+  
+  , functorWrappedSListIsSList
+  )where
 
 import           Generics.SOP          (hmap,hcollapse,NS(..),NP(..),SListI(..)
                                        ,SListI2,SList(..),All2,Compose
@@ -31,7 +68,7 @@ import Data.Functor.Identity           (Identity(Identity,runIdentity))
 import Data.Maybe                      (fromJust)
 
 
--- some preliminaries for type-level tags for the constructors of sum types
+-- |A Tag type for making DMaps of type-level lists
 data TypeListTag (xs :: [k]) (x :: k) where -- x is in xs
   Here  :: TypeListTag (x ': xs) x          -- x begins xs
   There :: TypeListTag xs x -> TypeListTag (y ': xs) x -- given that x is in xs, x is also in (y ': xs)
@@ -41,30 +78,25 @@ instance GEq (TypeListTag xs) where
   geq (There x) (There y) = geq x y
   geq _         _         = Nothing
 
-
-instance All2 (Compose Eq FieldInfo) xss=> DS.EqTag (TypeListTag xss) ConstructorInfo where
-  eqTagged Here      Here      = (==)
-
 instance GCompare (TypeListTag xs) where
   gcompare Here Here = GEQ
   gcompare Here (There _) = GLT
   gcompare (There _) Here = GGT
   gcompare (There x) (There y) = gcompare x y
 
-instance (All2 (Compose Eq FieldInfo) xss
-         ,All2 (Compose Ord FieldInfo) xss)=>DS.OrdTag (TypeListTag xss) ConstructorInfo where
-  compareTagged Here Here = compare
 
+
+-- |Convert an 'NP' indexed by typelist xs into a 'DM.DMap' indexed by 'TypeListTag' xs
 npToDMap::NP f xs -> DM.DMap (TypeListTag xs) f
 npToDMap Nil = DM.empty
 npToDMap (fx :* np') = DM.insert Here fx $ DM.mapKeysMonotonic There $ npToDMap np'
 
---NB: This can fail since there is no guarantee that the DMap has entries for every tag
---But, the length check is enough since the NP has to be the right length and the DMap entries are
---one per tag
+-- |Convert a 'DM.DMap' indexed by 'TypeListTag' xs to an 'NP'
+-- |NB: This can fail since there is no guarantee that the 'DM.DMap' has entries for every tag. Hence it returns a 'Maybe'
 dMapToNP::forall xs f.SListI xs=>DM.DMap (TypeListTag xs) f -> Maybe (NP f xs)
 dMapToNP dm = sequence'_NP $ hmap (\tag -> Comp $ DM.lookup tag dm) makeTypeListTagNP
 
+-- |Convert a 'NS' indexed by a typelist xs to a 'DS.DSum' indexed by 'TypeListTag' xs 
 nsToDSum::SListI xs=>NS f xs -> DS.DSum (TypeListTag xs) f
 nsToDSum ns =
   let nsFToNSDSum::SListI xs=>NS f xs -> NS (K (DS.DSum (TypeListTag xs) f)) xs
@@ -73,11 +105,18 @@ nsToDSum ns =
       tagsToFs = hmap (\tag -> (Fn $ \val -> K (tag :=> val)))
   in hcollapse $ nsFToNSDSum ns
 
+-- |Convert a 'DS.DSum' indexed by 'TypeListTag' xs into a 'NS' indexed by xs
 dSumToNS::SListI xs=>DS.DSum (TypeListTag xs) f -> NS f xs
 dSumToNS (tag :=> fa) = go tag fa where
   go::TypeListTag ys y->f y->NS f ys
   go Here fy = Z fy
   go (There tag') fy = S (go tag' fy)
+
+makeTypeListTagNP::SListI xs=>NP (TypeListTag xs) xs
+makeTypeListTagNP = go sList where
+  go::forall ys.SListI ys=>SList ys->NP (TypeListTag ys) ys
+  go SNil = Nil
+  go SCons = Here :* hmap There (go sList)
 
 
 -- these are here to allow moving functors in and out of typelists
@@ -96,25 +135,19 @@ npUnCompose np = go np where
   go (fgx :* np') = unComp fgx :* go np'
 
 
-npRecompose::forall f g xs.SListI xs=>NP f (FunctorWrapTypeList g xs) -> NP (f :.: g) xs -- (RemoveFunctor g (AddFunctor g xs))
-npRecompose = go sList where
+npReCompose::forall f g xs.SListI xs=>NP f (FunctorWrapTypeList g xs) -> NP (f :.: g) xs -- (RemoveFunctor g (AddFunctor g xs))
+npReCompose = go sList where
   go::forall ys.SListI ys=>SList ys ->  NP f (FunctorWrapTypeList g ys) -> NP (f :.: g) ys
   go SNil Nil = Nil
   go SCons (fgx :* np') = Comp fgx :* go sList np'
 
-nsOfnpRecompose::forall f g xss.(SListI xss, SListI2 xss)=>NS (NP f) (FunctorWrapTypeListOfLists g xss) -> NS (NP (f :.: g)) xss
-nsOfnpRecompose = go sList
+nsOfnpReCompose::forall f g xss.(SListI xss, SListI2 xss)=>NS (NP f) (FunctorWrapTypeListOfLists g xss) -> NS (NP (f :.: g)) xss
+nsOfnpReCompose = go sList
   where
     go::forall yss.(SListI2 yss, SListI yss)=>SList yss->NS (NP f) (FunctorWrapTypeListOfLists g yss) -> NS (NP (f :.: g)) yss
-    go SNil _ = undefined
-    go SCons (Z np) = Z (npRecompose np)
+    go SNil _ = undefined -- this shouldn't' happen since an NS can't be empty
+    go SCons (Z np) = Z (npReCompose np)
     go SCons (S ns') = S (go sList ns')
-
-makeTypeListTagNP::SListI xs=>NP (TypeListTag xs) xs
-makeTypeListTagNP = go sList where
-  go::forall ys.SListI ys=>SList ys->NP (TypeListTag ys) ys
-  go SNil = Nil
-  go SCons = Here :* hmap There (go sList)
 
 
 -- required to prove the wrapped typelist is an instance of SListI
@@ -136,4 +169,4 @@ npSequenceViaDMap::forall k (f:: * -> *)  (g:: * -> *) (xs::[*]).(Functor f
   =>(DM.DMap k f -> f (DM.DMap k Identity))->NP (f :.: g) xs -> f (NP g xs)
 npSequenceViaDMap sequenceDMap =
   withDict (functorWrappedSListIsSList (Proxy :: Proxy g) (sList :: SList xs)) $
-  fmap (hmap (runIdentity . unComp) . npRecompose . fromJust . dMapToNP) .  sequenceDMap . npToDMap . npUnCompose
+  fmap (hmap (runIdentity . unComp) . npReCompose . fromJust . dMapToNP) .  sequenceDMap . npToDMap . npUnCompose
